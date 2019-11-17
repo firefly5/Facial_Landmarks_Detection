6e4f06535da97ecb47003feb5c8f5f3988f319ef
adnil8130 <45663196+adnil8130@users.noreply.github.com>
2019-11-17 08:40:49 -0600

    Add files via upload

---
+++ Stage3_Hourglass_DSNT_Adam_Add_Classification/dsnt/model.py
@@ -0,0 +1,382 @@
+"""
+Code for building neural network models.
+"""
+
+import inspect
+import re
+
+import torch
+from torch import nn
+import torch.nn.functional
+from torch.autograd import Variable
+from torch.utils import model_zoo
+from torchvision import models
+
+import dsnt.nn
+from dsnt.nn import euclidean_loss, thresholded_softmax
+from dsnt import util, hourglass
+from dsnt.data import ImageSpecs
+
+
+class HumanPoseModel(nn.Module):
+    """Abstract base class for human pose estimation models."""
+
+    def _hm_preact(self, x, preact):
+        n_chans = x.size(-3)
+        height = x.size(-2)
+        width = x.size(-1)
+        # resize to (C, H * W)
+        x = x.view(-1, height * width)
+
+        # do softmax every row, totally C rows
+        if preact == 'softmax':
+            x = nn.functional.softmax(x, dim=-1)
+        elif preact == 'thresholded_softmax':
+            x = thresholded_softmax(x, -0.5)
+        elif preact == 'abs':
+            x = x.abs()
+            x = x / (x.sum(-1, keepdim=True) + 1e-12)
+        elif preact == 'relu':
+            x = nn.functional.relu(x, inplace=False)
+            x = x / (x.sum(-1, keepdim=True) + 1e-12)
+        elif preact == 'sigmoid':
+            x = nn.functional.sigmoid(x)
+            x = x / (x.sum(-1, keepdim=True) + 1e-12)
+        else:
+            raise Exception('unrecognised heatmap preactivation function: {}'.format(preact))
+        x = x.view(-1, n_chans, height, width)
+        return x
+
+    def _calculate_reg_loss(self, target_var, mask_var, reg, hm_var, hm_sigma):
+        # Convert sigma (aka standard deviation) from pixels to normalized units
+        sigma = (2.0 * hm_sigma / hm_var.size(-1))
+
+        # Apply a regularisation term relating to the shape of the heatmap.
+        if reg == 'var':
+            reg_loss = dsnt.nn.variance_reg_loss(hm_var, target_var, sigma, mask_var)
+        elif reg == 'kl':
+            reg_loss = dsnt.nn.kl_reg_loss(hm_var, target_var, sigma, mask_var)
+        elif reg == 'js':
+            reg_loss = dsnt.nn.js_reg_loss(hm_var, target_var, sigma, mask_var)
+        elif reg == 'mse':
+            reg_loss = dsnt.nn.mse_reg_loss(hm_var, target_var, sigma, mask_var)
+        else:
+            reg_loss = 0
+
+        return reg_loss
+
+    @property
+    def image_specs(self):
+        """Specifications of expected input images."""
+        raise NotImplementedError()
+
+    def forward_loss(self, out_var, target_var, mask_var):
+        """Calculate the value of the loss function."""
+        raise NotImplementedError()
+
+    def compute_coords(self, out_var):
+        """Calculate joint coordinates from the network output."""
+        raise NotImplementedError()
+
+
+class ResNetHumanPoseModel(HumanPoseModel):
+    """Create a ResNet-based model for human pose estimation.
+
+    Args:
+        resnet (nn.Module): ResNet model which will form the base of the model
+        n_chans (int): Number of output locations
+        dilate (int): Number of ResNet layer groups to use dilation for instead of downsampling
+        truncate (int): Number of ResNet layer groups to chop off
+        output_strat (str): Strategy for going between heatmaps and coords (dsnt, fc, gauss)
+    """
+
+    def __init__(self, resnet, n_chans=21, dilate=0, truncate=0, output_strat='dsnt',
+                 preact='softmax', reg='none', reg_coeff=1.0, hm_sigma=1.0):
+        super().__init__()
+
+        self.n_chans = n_chans
+        self.output_strat = output_strat
+        self.preact = preact
+        self.reg = reg
+        self.reg_coeff = reg_coeff
+        self.hm_sigma = hm_sigma
+
+        self.heatmap_size = 7 * 2 ** max(dilate, truncate)
+
+        fcn_modules = [
+            resnet.conv1,
+            resnet.bn1,
+            resnet.relu,
+            resnet.maxpool,
+            resnet.layer1,
+        ]
+        layers = [resnet.layer2, resnet.layer3, resnet.layer4]
+
+        for i, layer in enumerate(layers[len(layers) - dilate:]):
+            dilx = dily = 2 ** (i + 1)
+            for module in layer.modules():
+                if isinstance(module, nn.Conv2d):
+                    if module.stride == (2, 2):
+                        module.stride = (1, 1)
+                    elif module.kernel_size == (3, 3):
+                        kx, ky = module.kernel_size
+                        module.dilation = (dilx, dily)
+                        module.padding = ((dilx * (kx - 1) + 1) // 2, (dily * (ky - 1) + 1) // 2)
+
+        fcn_modules.extend(layers[:len(layers)-truncate])
+        self.fcn = nn.Sequential(*fcn_modules)
+        if truncate > 0:
+            feats = layers[-truncate][0].conv1.in_channels
+        else:
+            feats = resnet.fc.in_features
+        self.hm_conv = nn.Conv2d(feats, self.n_chans, kernel_size=1, bias=False)
+
+        if self.output_strat == 'fc':
+            self.out_fc = nn.Linear(self.heatmap_size * self.heatmap_size, 2)
+
+    @property
+    def image_specs(self):
+        return ImageSpecs(size=224, subtract_mean=False, divide_stddev=False)
+
+    def forward_loss(self, out_var, target_var, mask_var):
+        if self.output_strat == 'dsnt' or self.output_strat == 'fc':
+            loss = euclidean_loss(out_var, target_var, mask_var)
+
+            reg_loss = self._calculate_reg_loss(
+                target_var, mask_var, self.reg, self.heatmaps, self.hm_sigma)
+
+            return loss + self.reg_coeff * reg_loss
+        elif self.output_strat == 'fc':
+            return euclidean_loss(out_var, target_var, mask_var)
+        elif self.output_strat == 'gauss':
+            norm_coords = target_var.data.cpu()
+            width = out_var.size(-1)
+            height = out_var.size(-2)
+
+            target_hm = util.encode_heatmaps(norm_coords, width, height, self.hm_sigma)
+            target_hm_var = Variable(target_hm.cuda())
+
+            loss = nn.functional.mse_loss(out_var, target_hm_var)
+            return loss
+
+        raise Exception('invalid configuration')
+
+    def compute_coords(self, out_var):
+        if self.output_strat == 'dsnt' or self.output_strat == 'fc':
+            return out_var.data.type(torch.FloatTensor)
+        elif self.output_strat == 'gauss':
+            return util.decode_heatmaps(out_var.data.cpu())
+
+        raise Exception('invalid configuration')
+
+    def forward_part1(self, x):
+        """Forward from images to unnormalized heatmaps"""
+
+        x = self.fcn(x)
+        x = self.hm_conv(x)
+        return x
+
+    def forward_part2(self, x):
+        """Forward from unnormalized heatmaps to output"""
+
+        if self.output_strat == 'dsnt':
+            x = self._hm_preact(x, self.preact)
+            self.heatmaps = x
+            x = dsnt.nn.dsnt(x)
+        elif self.output_strat == 'fc':
+            x = self._hm_preact(x, self.preact)
+            self.heatmaps = x
+            height = x.size(-2)
+            width = x.size(-1)
+            x = x.view(-1, height * width)
+            x = self.out_fc(x)
+            x = x.view(-1, self.n_chans, 2)
+        else:
+            self.heatmaps = x
+
+        return x
+
+    def forward(self, *inputs):
+        x = inputs[0]
+        x = self.forward_part1(x)
+        x = self.forward_part2(x)
+
+        return x
+
+
+class HourglassHumanPoseModel(HumanPoseModel):
+    def __init__(self, hg, n_chans=21, output_strat='gauss', preact='softmax', reg='none',
+                 reg_coeff=1.0, hm_sigma=1.0):
+        super().__init__()
+
+        self.hg = hg
+        self.n_chans = n_chans
+        self.output_strat = output_strat
+        self.preact = preact
+        self.reg = reg
+        self.reg_coeff = reg_coeff
+        self.hm_sigma = hm_sigma
+
+        try:
+            self.heatmap_size = hg.heatmap_size
+        except AttributeError:
+            self.heatmap_size = 64
+
+        if self.output_strat == 'fc':
+            self.out_fc = nn.Linear(self.heatmap_size * self.heatmap_size, 2)
+
+    @property
+    def image_specs(self):
+        return ImageSpecs(size=256, subtract_mean=True, divide_stddev=False)
+
+    @property
+    def heatmaps(self):
+        return self.heatmaps_array[0]
+
+    def forward_loss(self, out_vars, target_var, mask_var):
+        if self.output_strat == 'dsnt' or self.output_strat == 'fc':
+            total_loss = 0
+
+            # Calculate the loss for each hourglass output, and take the total sum
+            for i, out_var in enumerate(out_vars):
+                loss = euclidean_loss(out_var, target_var, mask_var)
+
+                reg_loss = self._calculate_reg_loss(
+                    target_var, mask_var, self.reg, self.heatmaps_array[i], self.hm_sigma)
+
+                total_loss += loss + self.reg_coeff * reg_loss
+
+            return total_loss
+        elif self.output_strat == 'gauss':
+            norm_coords = target_var.data.cpu()
+            width = out_vars[0].size(-1)
+            height = out_vars[0].size(-2)
+
+            target_hm = util.encode_heatmaps(norm_coords, width, height, self.hm_sigma)
+            target_hm_var = Variable(target_hm.cuda())
+
+            # Calculate and sum up intermediate losses
+            loss = sum([nn.functional.mse_loss(hm, target_hm_var) for hm in out_vars])
+
+            return loss
+
+        raise Exception('invalid configuration')
+
+    def compute_coords(self, out_var):
+        if isinstance(out_var, list):
+            out_var = out_var[-1]
+
+        if self.output_strat == 'dsnt' or self.output_strat == 'fc':
+            return out_var.data.type(torch.FloatTensor)
+        elif self.output_strat == 'gauss':
+            return util.decode_heatmaps(out_var.data.cpu())
+
+        raise Exception('invalid configuration')
+
+    def forward_part1(self, x):
+        """Forward from images to unnormalized heatmaps"""
+
+        return self.hg(x)
+
+    def forward_part2(self, hg_outs):
+        """Forward from unnormalized heatmaps to output"""
+
+        out = []
+
+        if self.output_strat == 'gauss':
+            self.heatmaps_array = hg_outs
+            out = hg_outs
+        elif self.output_strat == 'dsnt':
+            self.heatmaps_array = []
+            for x in hg_outs:
+                x = self._hm_preact(x, self.preact)
+                self.heatmaps_array.append(x)
+                x = dsnt.nn.dsnt(x)
+                out.append(x)
+        elif self.output_strat == 'fc':
+            self.heatmaps_array = []
+            for x in hg_outs:
+                x = self._hm_preact(x, self.preact)
+                self.heatmaps_array.append(x)
+                height = x.size(-2)
+                width = x.size(-1)
+                x = x.view(-1, height * width)
+                x = self.out_fc(x)
+                x = x.view(-1, self.n_chans, 2)
+                out.append(x)
+        else:
+            raise Exception('invalid configuration')
+
+        return out
+
+    def forward(self, *inputs):
+        x = inputs[0]
+        x, c = self.forward_part1(x)
+        x = self.forward_part2(x)
+
+        return x, c
+
+
+def _build_resnet_pose_model(base, dilate=0, truncate=0, output_strat='dsnt', preact='softmax',
+                             reg='none', reg_coeff=1.0, hm_sigma=1.0):
+    """Create a ResNet-based pose estimation model with pretrained parameters.
+
+        Args:
+            base (str): Base ResNet model type (eg 'resnet34')
+            truncate (int): Number of ResNet layer groups to chop off
+            output_strat (str): Output strategy
+    """
+
+    if base == 'resnet18':
+        resnet = models.resnet18(pretrained=True)
+    elif base == 'resnet34':
+        resnet = models.resnet34(pretrained=True)
+    elif base == 'resnet50':
+        resnet = models.resnet50(pretrained=True)
+    elif base == 'resnet101':
+        resnet = models.resnet101(pretrained=True)
+    elif base == 'resnet152':
+        resnet = models.resnet152(pretrained=True)
+    else:
+        raise Exception('unsupported base model type: ' + base)
+
+    model = ResNetHumanPoseModel(
+        resnet, n_chans=21, dilate=dilate, truncate=truncate, output_strat=output_strat,
+        preact=preact, reg=reg, reg_coeff=reg_coeff, hm_sigma=hm_sigma)
+    return model
+
+
+def _build_hg_model(base, stacks=2, blocks=1, output_strat='gauss', preact='softmax',
+                    reg='none', reg_coeff=1.0, hm_sigma=1.0):
+    m = re.search('hg(\d+)', base)
+
+    if m is not None:
+        stacks = int(m.group(1))
+    elif base == 'hg':
+        pass
+    else:
+        raise Exception('unsupported base model type: ' + base)
+
+    hg = hourglass.HourglassNet(hourglass.Bottleneck, num_stacks=stacks, num_blocks=blocks)
+
+    model = HourglassHumanPoseModel(hg, n_chans=21, output_strat=output_strat, preact=preact,
+                                    reg=reg, reg_coeff=reg_coeff, hm_sigma=hm_sigma)
+    return model
+
+
+def build_mpii_pose_model(base='resnet34', **kwargs):
+    """Create a pose estimation model"""
+
+    if base.startswith('resnet'):
+        build_model = _build_resnet_pose_model
+    elif base.startswith('hg'):
+        build_model = _build_hg_model
+    else:
+        raise Exception('unsupported base model type: ' + base)
+
+    # Filter out unexpected parameters
+    func_params = inspect.signature(build_model).parameters.values()
+    param_names = [p.name for p in func_params if p.default != inspect.Parameter.empty]
+    kwargs = {k: kwargs[k] for k in param_names if k in kwargs}
+
+    return build_model(base, **kwargs)
---
+++ Stage3_Hourglass_DSNT_Adam_Add_Classification/dsnt/util.py
@@ -0,0 +1,234 @@
+"""
+Miscellaneous utility functions.
+"""
+
+import random
+import time
+from contextlib import contextmanager
+
+import math
+import numpy as np
+import torch
+from PIL.ImageDraw import Draw
+
+# Joints to connect for visualisation, giving the effect of drawing a
+# basic "skeleton" of the pose.
+BONES = {
+    'right_lower_leg': (0, 1),
+    'right_upper_leg': (1, 2),
+    'right_pelvis': (2, 6),
+    'left_lower_leg': (4, 5),
+    'left_upper_leg': (3, 4),
+    'left_pelvis': (3, 6),
+    'center_lower_torso': (6, 7),
+    'center_upper_torso': (7, 8),
+    'center_head': (8, 9),
+    'right_lower_arm': (10, 11),
+    'right_upper_arm': (11, 12),
+    'right_shoulder': (12, 8),
+    'left_lower_arm': (14, 15),
+    'left_upper_arm': (13, 14),
+    'left_shoulder': (13, 8),
+}
+
+
+def draw_skeleton(img, coords, joint_mask=None):
+    '''Draw a pose skeleton connecting joints (for visualisation purposes).
+
+    Left-hand-side joints are connected with blue lines. Right-hand-size joints
+    are connected with red lines. Center joints are connected with magenta
+    lines.
+
+    Args:
+        img (PIL.Image.Image): PIL image which the skeleton will be drawn over.
+        coords (Tensor): 16x2 tensor containing 0-based pixel coordinates
+            of joint locations. Joints indices are expected to match
+            http://human-pose.mpi-inf.mpg.de/#download
+        joint_mask (Tensor, optional): Mask of valid joints (invalid joints
+            will be drawn with grey lines).
+    '''
+
+    draw = Draw(img)
+    for bone_name, (j1, j2) in BONES.items():
+        if bone_name.startswith('center_'):
+            colour = (255, 0, 255) # Magenta
+        elif bone_name.startswith('left_'):
+            colour = (0, 0, 255) # Blue
+        elif bone_name.startswith('right_'):
+            colour = (255, 0, 0) # Red
+        else:
+            colour = (255, 255, 255)
+
+        if joint_mask is not None:
+            # Change colour to grey if either vertex is not masked in
+            if joint_mask[j1] == 0 or joint_mask[j2] == 0:
+                colour = (100, 100, 100)
+
+        draw.line([coords[j1, 0], coords[j1, 1], coords[j2, 0], coords[j2, 1]], fill=colour)
+
+
+def draw_gaussian(img_tensor, x, y, sigma, normalize=False, clip_size=None):
+    '''Draw a Gaussian in a single-channel 2D image.
+
+    Args:
+        img_tensor: Image tensor to draw to.
+        x: x-coordinate of Gaussian centre (in pixels).
+        y: y-coordinate of Gaussian centre (in pixels).
+        sigma: Standard deviation of Gaussian (in pixels).
+        normalize: Ensures values sum to 1 when True.
+        clip_size: Restrict the size of the draw region.
+    '''
+
+    # To me it makes more sense to round() these, but hey - I'm just following the example
+    # of others.
+    x = int(x)
+    y = int(y)
+
+    if img_tensor.dim() == 2:
+        height, width = list(img_tensor.size())
+    elif img_tensor.dim() == 3:
+        n_chans, height, width = list(img_tensor.size())
+        assert n_chans == 1, 'expected img_tensor to have one channel'
+        img_tensor = img_tensor[0]
+    else:
+        raise Exception('expected img_tensor to have 2 or 3 dimensions')
+
+    radius = max(width, height)
+    if clip_size is not None:
+        radius = clip_size / 2
+
+    if radius < 0.5 or x <= -radius or y <= -radius or \
+            x >= (width - 1) + radius or y >= (height - 1) + radius:
+        return
+
+    start_x = max(0, math.ceil(x - radius))
+    end_x = min(width, int(x + radius + 1))
+    start_y = max(0, math.ceil(y - radius))
+    end_y = min(height, int(y + radius + 1))
+    w = end_x - start_x
+    h = end_y - start_y
+
+    subimg = img_tensor[start_y:end_y, start_x:end_x]
+
+    xs = torch.arange(start_x, end_x).type_as(img_tensor).view(1, w).expand_as(subimg)
+    ys = torch.arange(start_y, end_y).type_as(img_tensor).view(h, 1).expand_as(subimg)
+
+    k = -0.5 * (1 / sigma)**2
+    subimg.copy_((xs - x)**2)
+    subimg.add_((ys - y)**2)
+    subimg.mul_(k)
+    subimg.exp_()
+
+    if normalize:
+        val_sum = subimg.sum()
+        if val_sum > 0:
+            subimg.div_(val_sum)
+
+
+def encode_heatmaps(coords, width, height, sigma=1):
+    '''Convert normalised coordinates into heatmaps.'''
+
+    # Normalised coordinates to pixel coordinates
+    coords.add_(1)
+    coords[:, :, 0].mul_(width / 2)
+    coords[:, :, 1].mul_(height / 2)
+    coords.add_(-0.5)
+
+    batch_size = coords.size(0)
+    n_chans = coords.size(1)
+    target = torch.FloatTensor(batch_size, n_chans, height, width).zero_()
+    for i in range(batch_size):
+        for j in range(n_chans):
+            x = round(coords[i, j, 0])
+            y = round(coords[i, j, 1])
+            draw_gaussian(target[i, j], x, y, sigma, normalize=False, clip_size=7)
+
+    return target
+
+
+def get_preds(heatmaps):
+    batch_size, n_chans, height, width = list(heatmaps.size())
+
+    maxval, idx = torch.max(heatmaps.view(batch_size, n_chans, -1), 2)
+
+    maxval = maxval.view(batch_size, n_chans, 1)
+    idx = idx.view(batch_size, n_chans, 1)
+
+    coords = idx.repeat(1, 1, 2)
+
+    coords[:, :, 0] = coords[:, :, 0] % width
+    coords[:, :, 1] = coords[:, :, 1] / height
+    coords = coords.float()
+
+    # When maxval is zero, select coords (0, 0)
+    pred_mask = maxval.gt(0).repeat(1, 1, 2).float()
+    torch.mul(coords, pred_mask, out=coords)
+
+    return coords
+
+
+def decode_heatmaps(heatmaps, use_neighbours=True):
+    '''Convert heatmaps into normalised coordinates.'''
+
+    coords = get_preds(heatmaps)
+
+    _, _, height, width = list(heatmaps.size())
+
+    if use_neighbours:
+        # "To improve performance at high precision thresholds the prediction
+        # is offset by a quarter of a pixel in the direction of its next highest
+        # neighbor before transforming back to the original coordinate space
+        # of the image"
+        #   - Stacked Hourglass Networks for Human Pose Estimation
+        for i, joint_coords in enumerate(coords):
+            for j, (x, y) in enumerate(joint_coords):
+                x = int(x)
+                y = int(y)
+                if x > 0 and x < width - 1 and y > 0 and y < height - 1:
+                    hm = heatmaps[i, j]
+                    joint_coords[j, 0] += (0.25 * np.sign(hm[y, x + 1] - hm[y, x - 1]))
+                    joint_coords[j, 1] += (0.25 * np.sign(hm[y + 1, x] - hm[y - 1, x]))
+
+    # Pixel coordinates to normalised coordinates
+    coords.add_(0.5)
+    coords[:, :, 0].mul_(2 / width)
+    coords[:, :, 1].mul_(2 / height)
+    coords.add_(-1)
+
+    return coords
+
+
+def type_as_index(indices, tensor):
+    if tensor.is_cuda:
+        return indices.type(torch.cuda.LongTensor)
+    return indices.type(torch.LongTensor)
+
+
+def reverse_tensor(tensor, dim):
+    indices = torch.arange(tensor.size(dim) - 1, -1, -1)
+    indices = type_as_index(indices, tensor)
+    return tensor.index_select(dim, indices)
+
+
+@contextmanager
+def timer(meter):
+    start_time = time.perf_counter()
+    yield
+    time_elapsed = time.perf_counter() - start_time
+    meter.add(time_elapsed)
+
+
+def generator_timer(generator, meter):
+    while True:
+        with timer(meter):
+            vals = next(generator)
+        yield vals
+
+
+def seed_random_number_generators(seed):
+    """Seed all random number generators."""
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
